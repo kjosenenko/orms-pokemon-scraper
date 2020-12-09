3c6d38700e159e10eabbb2a6fddabf5d321c1c97
KJ Osenenko <karljosenenko@gmail.com>
2020-12-09 14:27:29 -0700

    commit tree d53ca2277f60f4af1c21e3bcd25c5aae540281ce

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,18 @@
 class Pokemon
+
+    attr_accessor :name, :type, :id, :db
+
+    def initialize(id:, name:, type:, db:)
+        @id, @name, @type, @db = id, name, type, db
+    end
+
+    def self.save(name, type, db)
+        db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+    end
+
+    def self.find(id, db)
+        results = db.execute("SELECT * FROM pokemon WHERE id = ? LIMIT 1", id).flatten
+        Pokemon.new(id: results[0], name: results[1], type: results[2], db: db)
+    end
+
 end
